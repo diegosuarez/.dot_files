c5ff20d09a2d480103d3e03174eeb2c279bffce0
Diego Suarez <diego.suargarcia@gmail.com>
2014-05-22 13:22:54 +0200

    Pulido script de instalacion

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+cd
 sudo apt-get update && sudo apt-get install python-dev
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh
 vim +BundleInstall +qall
+
+ln -nfs ~/.dot_files/.vimrc ~/.vimrc
+ln -nfs ~/.dot_files/.bashrc ~/.bashrc 
+. ~/.bashrc
