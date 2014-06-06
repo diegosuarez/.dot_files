06ec853778df78668994e2807574607767a259ba
Diego Suárez <diego.suargarcia@gmail.com>
2014-06-06 12:39:53 +0200

    Mejorado script de instalacion para hacer los pasos en orden

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd
-sudo apt-get update && sudo apt-get install python-dev
+sudo apt-get update && sudo apt-get install python-dev cmake build-essential
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +BundleInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh
-vim +BundleInstall +qall
 
 ln -nfs ~/.dot_files/.vimrc ~/.vimrc
 ln -nfs ~/.dot_files/.bashrc ~/.bashrc 
